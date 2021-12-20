61615b1d16caa38cf24b3a5f25ed346fd44a35bf
Safa G <59778894+safag05@users.noreply.github.com>
2021-12-20 12:26:18 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  description = "The project ID of the project"
+}
+
+variable "region" {
+  description = "The region where the project resides"
+  default     = "us-east1"
+}
+
+variable "armor_name" {
+  description = "name of the environment"
+}
